d52aaa165854cb35a62e26b36eacc2eb88286f45
¢σ∂єχ <61060030+codexJoin@users.noreply.github.com>
2023-03-27 18:59:32 -0300

    Update scripts.php

--- barbeiro/conteudo/scripts.php
+++ barbeiro/conteudo/scripts.php
@@ -4,7 +4,7 @@ $base_url = "barbeiro/"
 
 <script src="js/all.js"> </script>
 <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
-<script src="js/bootstrap/jquery-3.3.1.slim.min.js"></script>
+
 <script src="js/bootstrap/popper.min.js"></script>
 <script src="js/bootstrap/bootstrap.min.js"></script>
 <script src="js/navbar.js"> </script>
@@ -17,6 +17,29 @@ $base_url = "barbeiro/"
     $(document).ready(function() {
         $('#tabela_servicos_agendados').DataTable({
             responsive: true,
+            "bJQueryUI": true,
+            "oLanguage": {
+                "sProcessing": "Processando...",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sZeroRecords": "Não foram encontrados resultados",
+                "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando de 0 até 0 de 0 registros",
+                "sInfoFiltered": "",
+                "sInfoPostFix": "",
+                "sSearch": "Buscar:",
+                "sUrl": "",
+                "oPaginate": {
+                    "sFirst": "Primeiro",
+                    "sPrevious": "Anterior",
+                    "sNext": "Seguinte",
+                    "sLast": "Último"
+                }
+            }
+        });
+
+        $('.tabelaCustom').DataTable({
+            responsive: true,
+            width: '100%',
             "bJQueryUI": true,
             "oLanguage": {
                 "sProcessing": "Processando...",
